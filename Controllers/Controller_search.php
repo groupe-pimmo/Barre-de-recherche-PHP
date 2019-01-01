e5831bf9881c0d2b999a287b1a5e173bccfff68d
Prisca <groupe.pimm@gmail.com>
2019-01-01 22:28:59 +0100

    ajout action search_pro pour rechercher un professionnel particulier

--- Controllers/Controller_search.php
+++ Controllers/Controller_search.php
@@ -1,13 +1,17 @@
 <?php
     class Controller_search extends Controller{
          
-        public function action_search_pro(){
+        public function action_default(){
+            $this->action_search_presta();
+        }
+
+        public function action_search_presta(){
 
             if (isset($_POST["cp"]) and trim($_POST["cp"]) !== "" 
-            and isset($_POST["formule"])){
+            and isset($_POST["prestation"]) and isset($_POST['formule'])){
                 
                 $infos = [];
-                $noms = ['cp', 'formule'];
+                $noms = ['cp','prestation','formule'];
 
                 foreach($noms as $v){
                     if (isset($_POST[$v]) and trim($_POST[$v]) !== "")
@@ -17,23 +21,34 @@
                 }
     
                 $m = Model::get_model(); //Récupération du modèle
-                $data = [
-                    'pro' => $m->search_pro($infos) //Réccupère tous les professionnels correspondants à la recherche
-                ];
-                //Si on a bien un prix nobel d'identifiant$_GET["id"]
-                if($data !== false ){ 
-                    $this->render("pro",$data);
-                } 
+                if($m->search_presta($infos) == false )
+                    $this->action_error("Pas de prestation correspondant à votre recherche");
                 else {
-                    $this->action_error("Pas de professionnel correspondant à votre recherche");
+                    $data = [
+                        'presta' => $m->search_presta($infos) //Réccupère toutes les prestations correspondants à la recherche
+                    ];
+                    $this->render("presta",$data);
                 }
-
             }
-
         }
+        
+        public function action_search_pro(){
 
+            if (isset($_POST["nom_prof"]) and trim($_POST["nom_prof"]) !== ""){
+                $infos = [];
+                $nom = "nom_prof";
+
+                $infos[$nom] = $_POST[$nom];
+                $m = Model::get_model(); //Récupération du modèle
+                if($m->search_pro($infos) == false )
+                    $this->action_error("Pas de professionnel correspondant à votre recherche");
+                else {
+                    $data = [
+                        'pro' => $m->search_pro($infos) //Réccupère tous les professionnels correspondants à la recherche
+                    ];
+                    $this->render("pro",$data);               
+                }
+            }
         }
-    
-
     }
 ?>
